473b57eac227458e49a88378d337b5153270e567
Nakul Jain <163165553+nakul139@users.noreply.github.com>
2025-07-29 18:07:55 +0530

    Update apiPaths.js

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://ai-interview-app-backend-6vue.onrender.com";
 export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register", // Signup
